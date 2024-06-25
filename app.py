1f0e41aa38f71c23ed3d61db6a32c19f890d2655
raulromerocastro21 <87458091+raulromerocastro21@users.noreply.github.com>
2024-06-25 15:00:25 -0600

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import os
 from flask import Flask, jsonify, send_from_directory, render_template, request
 import pandas as pd
-import joblib
 
 app = Flask(__name__)
-
-# Cargar el modelo entrenado
-model = joblib.load('model.pkl')
 
 @app.route('/')
 def index():
@@ -20,17 +16,17 @@ def static_files(path):
 def predict():
     try:
         form_data = request.form.to_dict()
-        # Convertir datos del formulario a DataFrame
+        # Convert form data to DataFrame
         input_data = pd.DataFrame([form_data])
-        # Convertir todos los valores a numéricos
+        # Convert all values to numeric
         input_data = input_data.apply(pd.to_numeric)
 
-        # Predecir usando el modelo cargado
+        # Assuming 'model' is loaded and 'nueva_prediccion' function exists
         prediction = model.predict(input_data)
         return jsonify({'prediction': prediction[0]})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Puerto predeterminado 5000
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
