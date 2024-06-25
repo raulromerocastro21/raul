15b913358ca917090d5feee54e4bd64f966bf0c8
raulromerocastro21 <87458091+raulromerocastro21@users.noreply.github.com>
2024-06-25 12:30:12 -0600

    app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))  # Puerto predeterminado 5000
     app.run(debug=True, host='0.0.0.0', port=port)
